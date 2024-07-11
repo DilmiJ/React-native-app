912a6e29900fbdb225c5642c2529a7797ac1ffd4
Krishan <krishan142536@gmail.com>
2024-07-11 21:01:15 +0530

    1.1

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../Components/Login'; // Import your login page component
+
+const Stack = createStackNavigator();
 
 const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default index
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // wait for 2 seconds
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>My App</Text>
+      </View>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+  },
+});
+
+export default index;
